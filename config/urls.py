b0532647ac6840521c948aa879d4472cf6d0271d
otabek <otabecktursunov@gmail.com>
2026-04-15 14:58:31 +0500

    new w

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
     path('players/', views.players_view, name='players'),
     path('tryouts/', views.tryouts_view, name='tryouts'),
     path('u-20-players/', views.u20_players_view, name='u-20'),
+    path('statistics/', views.StatisticsView.as_view(), name='stats'),
+    path('statistics/150-accurate-predictions/', views.Top150AccuratePredictionsView.as_view(), name='150-accurate-predictions'),
+    path('statistics/top-50-clubs-by-expenditure/', views.Top50ClubsByExpenditureView.as_view(), name='top-50-clubs-by-expenditure'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
+from django.db.models import ExpressionWrapper, Count, Max, Sum, F
+from django.db.models.functions import Round, Abs
 from django.shortcuts import render, get_object_or_404
+from django.views import View
 
 from main.models import Club, Transfer, Season, Player
 
@@ -58,3 +61,38 @@ def club_details_view(request, pk):
         'players': players
     }
     return render(request, 'club-details.html', context)
+
+
+class StatisticsView(View):
+    def get(self, request):
+        return render(request, 'stats.html')
+
+
+
+class Top150AccuratePredictionsView(View):
+    def get(self, request):
+        transfers = Transfer.objects.exclude(price_tft=0).filter(price__isnull=False, price_tft__isnull=False).annotate(
+            percent_of_accurate=Round(
+                Abs(
+                    (F('price') - F('price_tft')) / F('price') * 100,
+                ),
+                precision=2
+            )
+        ).order_by('percent_of_accurate')[:150]
+        context = {
+            'transfers': transfers
+        }
+        return render(request, 'stats/150-accurate-predictions.html', context)
+
+
+class Top50ClubsByExpenditureView(View):
+    def get(self, request):
+        clubs = Club.objects.annotate(
+            total_expenditure=Sum(
+                'import_transfers__price',
+            )
+        ).filter(total_expenditure__isnull=False).order_by('-total_expenditure')[:50]
+        context = {
+            'clubs': clubs
+        }
+        return render(request, 'stats/top-50-clubs-by-expenditure.html', context)
